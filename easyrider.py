fdc635b61283389437bcdb453e60aebed72235e6
june <djie627@gmail.com>
2023-06-06 18:53:26 +1000

    Stage 2/6: Correct syntax

--- easyrider.py
+++ easyrider.py
@@ -1,11 +1,12 @@
 """
-Stage 1/6: Checking the data type
+Stage 2/6: Correct syntax
 Objectives:
 The string containing the data in JSON format is passed to standard input.
-Check that the data types match.
-Check that the required fields are filled in.
-Display the information about the number of found errors in total and in each field.
-Keep in mind that there might be no errors at all.
+Check that the data format complies with the documentation.
+Only the fields that have such a requirement are relevant, i.e. stop_name, stop_type, a_time,
+so, please, count errors only for them.
+Like in the previous stage, print the information about the number of found errors in total and in each field.
+Remember that there might be no errors at all.
 The output should have the same formatting as shown in the example.
 """
 
@@ -17,9 +18,9 @@ class EasyRiderDataChecker:
     data_structure = {
         "bus_id": {"type": int, "format": r"\d+"},
         "stop_id": {"type": int, "format": r"\d+"},
-        "stop_name": {"type": str, "format": r"[\w\s\.]+"},
+        "stop_name": {"type": str, "format": r"[A-Z][a-zA-Z\s]+\s(?:Road|Avenue|Boulevard|Street)"},
         "next_stop": {"type": int, "format": r"\d+"},
-        "stop_type": {"type": str, "format": r"[SOF/s]?"},  # S, O, F
+        "stop_type": {"type": str, "format": r"[SOF\s]?"},  # S, O, F
         "a_time": {"type": str, "format": r"\b(?:[01]\d|2[0-3]):[0-5]\d\b"},  # HH:MM
     }
 
@@ -40,9 +41,10 @@ def main():
     data = input()
     checker = EasyRiderDataChecker(data)
     checker.check_data()
-    print(f"Type and required field validation: {sum(checker.errors.values())} errors")
+    print(f"Format validation: {sum(checker.errors.values())} errors")
     for key, value in checker.errors.items():
-        print(f"{key}: {value}")
+        if key in ("stop_name", "stop_type", "a_time"):
+            print(f"{key}: {value}")
 
 
 if __name__ == '__main__':
